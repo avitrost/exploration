bc40f93e5df3d49e32bc4aa2310e1a69d6f4c834
avitrost <avitrost@gmail.com>
2025-03-19 18:32:06 -0600

    update

--- scripts/explore.sh
+++ scripts/explore.sh
@@ -5,10 +5,10 @@ export DATA_DIR=$DATA_ROOT/$TASK
 
 python3 examples/data_preprocess/preprocess_small_math.py --local_dir $DATA_DIR
 
-export N_GPUS=1
-export BASE_MODEL=Qwen/Qwen2.5-0.5B-Instruct
-export ROLLOUT_TP_SIZE=1
-export EXPERIMENT_NAME=ppo-$TASK-qwen2.5-0.5B-Instruct
+export N_GPUS=2
+export BASE_MODEL=atrost/qwen_3b_rl_math_2_epochs
+export ROLLOUT_TP_SIZE=2
+export EXPERIMENT_NAME=ppo-$TASK-qwen2.5-3B-$JOB_NUM
 
 
 python3 -m verl.trainer.main_ppo \
@@ -31,13 +31,13 @@ python3 -m verl.trainer.main_ppo \
     critic.model.path=$BASE_MODEL \
     critic.ppo_micro_batch_size=2 \
     algorithm.kl_ctrl.kl_coef=0.001 \
-    trainer.logger=['console'] \
+    trainer.logger=['console','wandb'] \
     +trainer.val_before_train=True \
     trainer.default_hdfs_dir=null \
     trainer.n_gpus_per_node=$N_GPUS \
     trainer.nnodes=1 \
     trainer.save_freq=0 \
-    trainer.test_freq=1 \
+    trainer.test_freq=10000 \
     trainer.project_name=math \
     trainer.experiment_name=$EXPERIMENT_NAME \
-    trainer.total_epochs=3 2>&1 | tee verl_demo.log
+    trainer.total_epochs=1 2>&1 | tee verl_demo.log
