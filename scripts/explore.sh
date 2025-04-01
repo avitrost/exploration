e99440692fb99d7e347b3de81b97117dae6cbf75
avitrost <avitrost@gmail.com>
2025-04-01 18:41:02 -0500

    update config for 7b

--- scripts/explore.sh
+++ scripts/explore.sh
@@ -5,23 +5,23 @@ export DATA_DIR=$DATA_ROOT/$TASK
 
 python3 examples/data_preprocess/aime.py --local_dir $DATA_DIR
 
-export N_GPUS=3
+export N_GPUS=4
 export BASE_MODEL=Qwen/Qwen2.5-Math-7B
-export ROLLOUT_TP_SIZE=1
+export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=ppo-$TASK-Qwen2.5-Math-7B-$JOB_NUM
 
 
 python3 -m verl.trainer.main_ppo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
-    data.train_batch_size=30 \
-    data.val_batch_size=30 \
+    data.train_batch_size=28 \
+    data.val_batch_size=28 \
     data.max_prompt_length=1024 \
     data.max_response_length=1024 \
     data.filter_overlong_prompts=True \
     actor_rollout_ref.model.path=$BASE_MODEL \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=30 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=28 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
