0d9ddc788725de25613fa99357f1fb22c6e9563e
avitrost <avitrost@gmail.com>
2025-08-11 12:26:24 -0500

    fix submodule

--- scripts/explore.sh
+++ scripts/explore.sh
@@ -27,7 +27,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.ref.use_dynamic_bsz=True \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
     critic.optim.lr=1e-5 \
     critic.model.path=$BASE_MODEL \
     critic.ppo_micro_batch_size_per_gpu=1 \
