a01531e1e6595093260d719f44702eac5cf7da51
avitrost <avitrost@gmail.com>
2025-08-11 12:32:32 -0500

    fix submodule

--- aime_1.py
+++ aime_1.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     # 'lighteval/MATH' is no longer available on huggingface.
     # Use mirror repo: DigitalLearningGmbH/MATH-lighteval
-    data_source = 'Maxwell-Jia/AIME_2024'
+    data_source = 'aime-Maxwell-Jia/AIME_2024'
     print(f"Loading the {data_source} dataset from huggingface...", flush=True)
     dataset = datasets.load_dataset(data_source, trust_remote_code=True)
 
